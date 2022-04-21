036e0033a2d124be01a00c2b3149231620b39a52
Emily Ury <ury.emily@gmail.com>
2022-04-21 15:14:57 -0400

    retention calculations and histograms

--- BMP_Phos_data_exploration.R
+++ BMP_Phos_data_exploration.R
@@ -3,9 +3,10 @@
 ## TP data exploration
 
 setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/BMP_project")
+setwd("C:/Users/uryem/OneDrive - University of Waterloo/BMP_project")
 
 library(ggplot2)
-
+options(scipen=999)
 
 TP <- read.csv("BMP_SUMMARY_TP.csv", head = TRUE)
 SRP <- read.csv("BMP_SUMMARY_OrthoP.csv", head = TRUE)
@@ -31,9 +32,21 @@ data <- rbind(TP, SRP, TPdis)
 data$retention_percent <- (data$Inflow_mg_L - data$Outflow_mg_L) / data$Inflow_mg_L *100
 data$flow_atten <- (data$Inflow_vol_m3-data$Outflow_vol_m3) / data$Inflow_vol_m3
 
+data$Load_In <- data$Inflow_mg_L*data$Inflow_vol_m3
+data$Load_Out <- data$Outflow_mg_L*data$Outflow_vol_m3
+data$retention <- data$Load_In - data$Load_Out
+
+
+d <- data[which(data$retention != 0 & data$retention > -10 & data$retention < 10),]
+
+hist(d$retention, breaks = 50)
+
+
 ### Remove rows where Inflow = Outflow
 nrow(data[which(data$flow_atten == 0),])
 data2 <- data[which(data$flow_atten != 0), ]
+
+data2$Species <- as.factor(data2$Species)
 
 plot(data2$flow_atten, data2$retention_percent, 
      ylim = c(-25,1), xlim = c(-25,1), 
