6ddff54f3c2215ffd126710b5e7731e9414c5e0c
EmilyUry <ury.emily@gmail.com>
2022-05-11 12:18:13 -0400

    new filters

--- BMP_Phos_data_exploration.R
+++ BMP_Phos_data_exploration.R
@@ -8,52 +8,35 @@ setwd("C:/Users/uryem/OneDrive - University of Waterloo/BMP_project")
 library(ggplot2)
 options(scipen=999)
 
-TP <- read.csv("BMP_SUMMARY_TP.csv", head = TRUE)
-SRP <- read.csv("BMP_SUMMARY_OrthoP.csv", head = TRUE)
-TPdis <- read.csv("BMP_SUMMARY_TPdis.csv", head = TRUE)
+data <- read.csv("BMP_SUMMARY_P_all.csv")
 
-
-head(TP)
-table(TP$BMPType)
-do.call("rbind",lapply(unique(TP$BMPType),function(b){
-  data.frame(BMPType=b,
-    num_sites=length(unique(subset(TP,BMPType==b)$BMPID))
-  )	
-}
-))
-
-
-
-### TP vs PO4 retention
-
-data <- rbind(TP, SRP, TPdis)
 
 ## calculate retention percent
-data$retention_percent <- (data$Inflow_mg_L - data$Outflow_mg_L) / data$Inflow_mg_L *100
-data$flow_atten <- (data$Inflow_vol_m3-data$Outflow_vol_m3) / data$Inflow_vol_m3
+data$flow_atten <- (data$Inflow_vol_m3-data$Outflow_vol_m3) 
+data$flow_atten_percent <- (data$Inflow_vol_m3-data$Outflow_vol_m3) / data$Inflow_vol_m3 *100
 
 data$Load_In <- data$Inflow_mg_L*data$Inflow_vol_m3
 data$Load_Out <- data$Outflow_mg_L*data$Outflow_vol_m3
 data$retention <- data$Load_In - data$Load_Out
+data$retention_percent <- (data$Load_In - data$Load_Out)/data$Load_In*100
 
 
-d <- data[which(data$retention != 0 & data$retention > -10 & data$retention < 10),]
+data3 <- data[which(data$flow_atten_percent != 0 & data$flow_atten_percent > -30 & data$flow_atten_percent < 90),]
 
-hist(d$retention, breaks = 50)
+hist(data3$retention, breaks = 500,
+     xlim = c(-50, 50))
 
 
 ### Remove rows where Inflow = Outflow
-nrow(data[which(data$flow_atten == 0),])
-data2 <- data[which(data$flow_atten != 0), ]
 
-data2$Species <- as.factor(data2$Species)
+data3$Species <- as.factor(data3$Species)
 
-plot(data2$flow_atten, data2$retention_percent, 
-     ylim = c(-25,1), xlim = c(-25,1), 
-     col = c('red', 'blue', 'green')[as.factor(data2$Species)], 
+plot(data3$flow_atten_percent, data3$retention_percent, 
+     ylim = c(-100,100), # xlim = c(-25,1), 
+     col = c('red', 'blue', 'green')[as.factor(data3$Species)], 
      pch = 16)
-legend("bottomleft", levels(data2$Species), pch = 16,  col = c('red', 'blue', 'green'))
-
+legend("bottomright", levels(data3$Species), pch = 16,  col = c('red', 'blue', 'green'))
+abline(a=0, b = 1)
 
 
 data.wide <- data %>%
