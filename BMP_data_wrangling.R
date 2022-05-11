36b349f36ed25121db415bfd7a9ac07756e42a60
EmilyUry <ury.emily@gmail.com>
2022-05-11 11:33:55 -0400

    remove DateStart

--- BMP_data_wrangling.R
+++ BMP_data_wrangling.R
@@ -84,17 +84,19 @@ P.key <- read.csv("Phos_Key.csv", head = TRUE) %>%
   select(c("old_names", "new_names"))
 
 Phos <- read.csv("WaterQuality.csv", header = T)  %>%
-  select('SiteID','MSID','EventID','DateSample', #'TimeSample',
+  select('SiteID','MSID','EventID',  'SampleMedia',  #'DateSample', #'TimeSample',
          'Analyte','Value_SubHalfDL', 'SampleFraction', 'Value_Unit',         ## might want to double check WQQualifer and SampleFraction
          'SampleType') %>%
+  filter(SampleMedia == "Surface Runoff/Flow") %>%
   filter(SampleType == 'EMC-Flow Weighted') %>%
   filter(Value_SubHalfDL >= -0.001) %>%
   filter(Analyte %in% c("Phosphorus as P", "Phosphorus, orthophosphate as P", "Orthophosphate", 
-                        "Phosphorus", "Phosphorus, orthophosphate as PO4", "Phosphorus, Particulate Organic", 
-                        "Phosphorus, organic as P")) %>%
+                        "Phosphorus", "Phosphorus, orthophosphate as PO4")) %>%
   mutate(Analyte_SampleType = paste(Analyte, SampleFraction)) %>%
-  select(-c("SampleFraction", "Analyte")) %>%
+  select(-c("SampleFraction", "Analyte", "SampleMedia")) %>%
   left_join(P.key, by = c("Analyte_SampleType" = "old_names")) 
+
+
 
 # table(Phos$Analyte_SampleType)
 table(Phos$new_names)
@@ -118,9 +120,10 @@ Phos <- left_join(Phos, key2, by = c("new_names" = "old_names2"))
 Phos <- Phos %>%
   select(-c("Value_SubHalfDL", "Analyte_SampleType", "SampleType", "new_names"))
 
-## average duplicate data points
-Phos <- aggregate(Value_harm ~ SiteID + MSID + EventID + DateSample + 
-                    #TimeSample 
+
+###average duplicate data points
+Phos <- aggregate(Value_harm ~ SiteID + MSID + EventID + 
+                    #DateSample + TimeSample 
                   + Value_Unit + Analyte_harm, data = Phos, FUN = mean)
 
 
@@ -148,7 +151,7 @@ TP.all <- flow.ID %>%
 OrthoP.all <- flow.ID %>%
   left_join(Phos.wide, by = c("SiteID", "MSID", "EventID")) %>%
   drop_na('ortho-P') 
-names(OrthoP.all)[15] <- "OP"  ## the hyphen causes issues later down the line
+names(OrthoP.all)[14] <- "OP"  ## the hyphen causes issues later down the line
 
 
 TP.dis.all <- flow.ID %>%
@@ -164,8 +167,8 @@ TP.dis.all <- flow.ID %>%
 
 TP.wide <- TP.all[-3,] %>%              
   select(-c("ortho-P", "TP.dis")) %>%
-  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", "DateStart", 
-                          #"TimeStart", "TimeEnd",
+  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", 
+                          #"DateStart", "TimeStart", "TimeEnd",
                           "Value_Unit"), ### broke this by including Timestart and Time end
               names_from = 'MSType', values_from = c("Volume_Total", "TP"))  %>%
   mutate(C1 = as.character(TP_Inflow), C2 = as.character(TP_Outflow), C3 = as.character(Volume_Total_Inflow), 
@@ -178,13 +181,13 @@ TP.wide <- TP.all[-3,] %>%
   relocate(Volume_Total_Outflow, .after = Volume_Total_Inflow)
 
 
-TP.final <- TP.wide[complete.cases(TP.wide[,10]),] ## TP_Outflow
-TP.final <- TP.final[complete.cases(TP.final[,9]),] ## TP_Inflow
-names(TP.final)[7] <- "Inflow_vol_m3"
-names(TP.final)[8] <- "Outflow_vol_m3"
+TP.final <- TP.wide[complete.cases(TP.wide[,9]),] ## TP_Outflow_mg_L
+TP.final <- TP.final[complete.cases(TP.final[,8]),] ## TP_Inflow_mg_L
+names(TP.final)[6] <- "Inflow_vol_m3"
+names(TP.final)[7] <- "Outflow_vol_m3"
 TP.final$Species <- "TP"
-names(TP.final)[9] <- "Inflow_mg_L"
-names(TP.final)[10] <- "Outflow_mg_L"
+names(TP.final)[8] <- "Inflow_mg_L"
+names(TP.final)[9] <- "Outflow_mg_L"
 
 #write.csv(TP.final, file = "BMP_SUMMARY_TP.csv")
 
@@ -193,7 +196,7 @@ names(TP.final)[10] <- "Outflow_mg_L"
 
 OrthoP.wide <- OrthoP.all[-3,] %>%              
   select(-c("TP", "TP.dis")) %>%
-  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", "DateStart", "Value_Unit"), 
+  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", "Value_Unit"), 
               names_from = 'MSType', values_from = c("Volume_Total", "OP")) %>%
   mutate(C1 = as.character(OP_Outflow), C2 = as.character(OP_Inflow), C3 = as.character(Volume_Total_Inflow), 
          C4 = as.character(Volume_Total_Outflow)) %>%
@@ -205,12 +208,13 @@ OrthoP.wide <- OrthoP.all[-3,] %>%
   relocate(Volume_Total_Outflow, .after = Volume_Total_Inflow)
 
 
-OrthoP.final <- na.omit(OrthoP.wide)
-names(OrthoP.final)[7] <- "Inflow_vol_m3"
-names(OrthoP.final)[8] <- "Outflow_vol_m3"
+OrthoP.final <- OrthoP.wide[complete.cases(OrthoP.wide[,9]),] ## Outflow_mg_L
+OrthoP.final <- OrthoP.final[complete.cases(OrthoP.final[,8]),] ## Inflow_mg_L
+names(OrthoP.final)[6] <- "Inflow_vol_m3"
+names(OrthoP.final)[7] <- "Outflow_vol_m3"
 OrthoP.final$Species <- "OrthoP"
-names(OrthoP.final)[9] <- "Inflow_mg_L"
-names(OrthoP.final)[10] <- "Outflow_mg_L"
+names(OrthoP.final)[8] <- "Inflow_mg_L"
+names(OrthoP.final)[9] <- "Outflow_mg_L"
 
 #write.csv(OrthoP.final, file = "BMP_SUMMARY_OrthoP.csv")
 
@@ -220,7 +224,7 @@ names(OrthoP.final)[10] <- "Outflow_mg_L"
 
 TPdis.wide <- TP.dis.all[-3,] %>%              
   select(-c("TP", "ortho-P"))  %>%
-  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", "DateStart", "Value_Unit"), 
+  pivot_wider(id_cols = c("SiteID", "BMPID", "BMPType", "EventID", "Value_Unit"), 
               names_from = 'MSType', values_from = c("Volume_Total", "TP.dis")) %>%
   mutate(C1 = as.character(TP.dis_Outflow), C2 = as.character(TP.dis_Inflow), C3 = as.character(Volume_Total_Inflow), 
          C4 = as.character(Volume_Total_Outflow)) %>%
@@ -231,12 +235,13 @@ TPdis.wide <- TP.dis.all[-3,] %>%
   relocate(TP.dis_Outflow, .after = TP.dis_Inflow)%>%
   relocate(Volume_Total_Outflow, .after = Volume_Total_Inflow)
 
-TPdis.final <- na.omit(TPdis.wide)
-names(TPdis.final)[7] <- "Inflow_vol_m3"
-names(TPdis.final)[8] <- "Outflow_vol_m3"
+TPdis.final <- TPdis.wide[complete.cases(TPdis.wide[,9]),] ## Outflow_mg_L
+TPdis.final <- TPdis.final[complete.cases(TPdis.final[,8]),] ## Inflow_mg_L
+names(TPdis.final)[6] <- "Inflow_vol_m3"
+names(TPdis.final)[7] <- "Outflow_vol_m3"
 TPdis.final$Species <- "TPdis"
-names(TPdis.final)[9] <- "Inflow_mg_L"
-names(TPdis.final)[10] <- "Outflow_mg_L"
+names(TPdis.final)[8] <- "Inflow_mg_L"
+names(TPdis.final)[9] <- "Outflow_mg_L"
 
 #write.csv(TPdis.final, file = "BMP_SUMMARY_TPdis.csv")
 
