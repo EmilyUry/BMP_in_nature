033068caee16bf172adca3a2640fb97619b3c0bd
Emily Ury <ury.emily@gmail.com>
2022-08-01 12:36:15 -0400

    cleaned and commented

--- Site_maps.R
+++ Site_maps.R
@@ -4,7 +4,7 @@
 ### map data
 
 ### 
-# Clean code for working up the clean data and running some comparative analyses
+# Mapping BMP Type by US Region
 
 setwd("C:/Users/Emily Ury/OneDrive - University of Waterloo/BMP_project")
 setwd("C:/Users/uryem/OneDrive - University of Waterloo/BMP_project")
@@ -12,6 +12,10 @@ setwd("C:/Users/uryem/OneDrive - University of Waterloo/BMP_project")
 library(tidyverse)
 library(ggplot2)
 library(viridis)
+#library(usmap)
+library(maps)
+library(scatterpie)
+
 options(scipen=999)
 
 x <- read.csv("BMP_Clean_filtered.csv", stringsAsFactors = TRUE)
@@ -24,29 +28,29 @@ levels(x$BMPType_name) <- BMP_names
 x$Species <- factor(x$Species , levels=c("NH4", "NO3","TKN", "TN" , "TP", "PO4"))
 
 
-plot(x$Long, x$Lat)
-levels(x$State)
 
+### 6 regions
 x$Region <- ifelse(x$State == "NH" | x$State == "NY" | x$State == "PA", "North East", 
                    ifelse(x$State == "VA" | x$State == "WV" | x$State == "MD" |x$State == "DE" | x$State == "NC" , "Mid Atlantic",
                           ifelse(x$State == "AL" | x$State == "GA" |x$State == "FL", "South East",
                                  ifelse(x$State == "OR" | x$State == "WA", "PNW",
                                         ifelse(x$State == "CO" | x$State == "CA"  | x$State == "TX", "South West", "Mid West")))))
 
-### or simplified
-x$Region <- ifelse(x$State == "VA" | x$State == "WV" | x$State == "MD" |x$State == "DE" |
-                     x$State == "NH" | x$State == "NY" | x$State == "PA", "East", 
-                   ifelse(x$State == "TX" | x$State == "AL" | x$State == "GA" |
-                            x$State == "NC" |x$State == "FL", "South",
-                          ifelse(x$State == "OR" | x$State == "CO" | x$State == "CA" | x$State == "WA", "West",
-                                 "Mid West")))
+# ### or simplified 4 regions
+# x$Region <- ifelse(x$State == "VA" | x$State == "WV" | x$State == "MD" |x$State == "DE" |
+#                      x$State == "NH" | x$State == "NY" | x$State == "PA", "East", 
+#                    ifelse(x$State == "TX" | x$State == "AL" | x$State == "GA" |
+#                             x$State == "NC" |x$State == "FL", "South",
+#                           ifelse(x$State == "OR" | x$State == "CO" | x$State == "CA" | x$State == "WA", "West",
+#                                  "Mid West")))
 
+## look at how the data breaks down by region
 x$Region <- as.factor(x$Region)
 levels(x$Region)
 table(x$State)
 table(x$Region)
 
-
+### summarize number of BMP Type in each region
 data <- x %>%
   select(c("SiteID", "BMPID", "BMPType", "BMPType_name" , "State", "Region", "Lat", "Long")) %>%
   unique() %>%
@@ -55,20 +59,18 @@ data <- x %>%
 table(data$state)
 table(data$Region)
 m <- table( data$Region, data$BMPType)
-
 m <- as.data.frame.matrix(m)
+
+
+## assign lat and long for each pie chart
 m$Long <- c(-76, -90, -70, -120, -86, -110)
 m$Lat <- c(35, 41, 45, 45, 31, 35)
 m$Total <- m$DB + m$RP + m$WB +m$WC + m$`BS/BI` + m$BR
 m
 
 
-library(usmap)
-library(ggplot2)
-library(maps)
-library(scatterpie)
 
-
+### Call in US state data and sort into custom regions
 States <- map_data("state")
 NE <- filter(States, region ==  "new york" | region ==
                                 "vermont" | region ==  "new hampshire" | region ==
@@ -92,17 +94,17 @@ PNW <- filter(States, region ==  "washington" | region == "oregon"|
 
 
 ggplot(States, aes (long, lat)) +
-  geom_map(map = States, aes(map_id=(region)), fill = "grey97", color = "grey20") +
-  geom_map(map = NE, aes(map_id=(region)), fill = "gray77", color = "grey20")+
+  geom_map(map = States, aes(map_id=(region)), fill = "grey97", color = "grey20") +  #US BASE MAP
+  geom_map(map = NE, aes(map_id=(region)), fill = "gray77", color = "grey20")+       ## Custom regions
   geom_map(map = MA, aes(map_id=(region)), fill = "gray55", color = "grey20")+
   geom_map(map = SE, aes(map_id=(region)), fill = "gray77", color = "grey20")+
   geom_map(map = SW, aes(map_id=(region)), fill = "gray55", color = "grey20")+
   geom_map(map = PNW, aes(map_id=(region)), fill = "gray77", color = "grey20")+
-  geom_point(data = data, aes(x = Long, y = Lat)) +
+  geom_point(data = data, aes(x = Long, y = Lat)) +                               ## BMPs as points
   xlim(-125, -65) +
   ylim(25,50) +
-  geom_scatterpie(data = m, 
-                  aes(Long, Lat, r = Total/7),
+  geom_scatterpie(data = m,                                                      ## add pie charts
+                  aes(Long, Lat, r = Total/7),                                   ## 'r' scales the pies # of BMPs in a region
                       cols = c("DB", "RP", "WB", "WC", "BS/BI", "BR"),
                       alpha = 0.5) +
   scale_fill_manual(labels = names,
@@ -120,9 +122,6 @@ ggplot(States, aes (long, lat)) +
         axis.ticks = element_blank())
   
 
-names <- levels(x$BMPType_name)
 
-
-table(x$State, x$Region)
 
 
