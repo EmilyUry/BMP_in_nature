8dc3e9fc9197ba08623dd41b78ce0328640af78e
Emily Ury <ury.emily@gmail.com>
2022-10-05 09:28:13 -0400

    fixed color order issue

--- Figure_N_v_P.R
+++ Figure_N_v_P.R
@@ -17,12 +17,12 @@ select <- read.csv("BMP_Clean_filtered.csv", stringsAsFactors = TRUE)
 levels(select$BMPType)
 
 ### reorder factors
-select$BMPType <- factor(select$BMPType , levels=c('DB','RP','WB', "WC",  "BS/BI", "BR"))
-levels(select$BMPType) <- c("DB", "RP", "WB", "WC", "GS", "BR")
-BMP_names <- c("Detention basin (dry)", "Retention pond", "Wetland basin", "Wetland channel", "Grass strip/swale", "Bioretention")
-levels(select$BMPType_name) <- BMP_names
-select$Species <- factor(select$Species , levels=c("NH4", "NO3","TKN", "TN" , "TP", "PO4"))
 
+select$BMPType <- factor(select$BMPType , levels=c('BR','BS/BI','DB', "RP",  "WB", "WC"))
+levels(select$BMPType) <- c('BR','GS','DB', "RP",  "WB", "WC")
+BMP_names <- c("Bioretention","Grass strip/swale", "Detention basin (dry)", "Retention pond", "Wetland basin", "Wetland channel")
+levels(select$BMPType_name) <- BMP_names
+select$Species <- factor(select$Species , levels=c("TN","NH4", "NO3","TKN", "TP", "PO4"))
 
 data <- select[which(select$Species != "TKN"),]
 
@@ -137,17 +137,17 @@ CC <- data %>%
   pivot_wider(names_from = Species, values_from = c("retention", "retention_percent"))
 new <- CC %>% drop_na
 new <- new %>%
-  mutate(quadrant = case_when(retention_percent_TN >= 0 & retention_percent_TP >= 0 
-                            & retention_percent_TN >= retention_percent_TP        ~ "aQuad 1under",
-                            retention_percent_TN >= 0 & retention_percent_TP >= 0 
-                            & retention_percent_TN < retention_percent_TP        ~ "fQuad 1over",                              
-                            retention_percent_TN <= 0 & retention_percent_TP >= 0~ "eQuad 2",
-                            retention_percent_TN <= 0 & retention_percent_TP <= 0 
-                            & retention_percent_TN >= retention_percent_TP        ~ "cQuad 3under",
-                            retention_percent_TN <= 0 & retention_percent_TP <= 0 
-                            & retention_percent_TN < retention_percent_TP        ~ "dQuad 3over",
-                            retention_percent_TN >= 0 & retention_percent_TP <= 0 ~ "bQuad 4",
-                            retention_percent_TN == retention_percent_TP ~ 'SAME'))
+  mutate(quadrant = case_when(retention_percent_TP >= 0 & retention_percent_TN >= 0 
+                            & retention_percent_TP >= retention_percent_TN        ~ "aQuad 1under",
+                            retention_percent_TP >= 0 & retention_percent_TN >= 0 
+                            & retention_percent_TP < retention_percent_TN        ~ "fQuad 1over",                              
+                            retention_percent_TP <= 0 & retention_percent_TN >= 0~ "eQuad 2",
+                            retention_percent_TP <= 0 & retention_percent_TN <= 0 
+                            & retention_percent_TP >= retention_percent_TN        ~ "cQuad 3under",
+                            retention_percent_TP <= 0 & retention_percent_TN <= 0 
+                            & retention_percent_TP < retention_percent_TN        ~ "dQuad 3over",
+                            retention_percent_TP >= 0 & retention_percent_TN <= 0 ~ "bQuad 4",
+                            retention_percent_TP == retention_percent_TN ~ 'SAME'))
 
 TN.TP <- ggplot(new, aes(x = BMPType, fill = quadrant)) +
   geom_bar(width = 0.75, position = 'fill',color = 'black') +
@@ -347,7 +347,7 @@ TN.NH4 <- ggplot(new6, aes(x = BMPType, fill = quadrant)) +
 
 
 
-tiff(filename = "figures/Solute_retention_supp.tif", height=4, width=6, units= "in", res=800, compression= "lzw")
+tiff(filename = "figures/Solute_retention5.tif", height=4, width=6, units= "in", res=800, compression= "lzw")
 
 plot_grid(inset, TN.TP, PO4.TP, NO3.NH4, TN.NO3, TN.NH4,
           labels = c("A) ","B) TN vs TP", "C) TP vs PO4 ", 
@@ -361,65 +361,92 @@ dev.off()
 
 
 
-plot(new5$retention_percent_TN, new5$retention_percent_NO3, ylim = c(-100,100), xlim = c(-100,100))
-abline(1,0)
-abline(0,1)
-
-plot(new6$retention_percent_TN, new6$retention_percent_NH4, ylim = c(-100,100), xlim = c(-100,100))
-abline(1,0)
-abline(0,1)
-
-
 
 
 
 
 #### Supplement2 Unscaled versions
 
-
-TN.TP <- ggplot(new, aes(x = BMPType, fill = quadrant)) +
+TN.TP2 <- ggplot(new, aes(x = BMPType, fill = quadrant)) +
   geom_bar(width = 0.75,color = 'black') +
   theme_classic(base_size = 10) +
-  scale_fill_manual(breaks = c("Quad 1over", "Quad 1under", "Quad 2",
-                               "Quad 3over", "Quad 3under", "Quad 4"), 
-                    values=c("#CCF1F6", "#8AD6E0", "#E2D1F1",
-                             '#FDDAE4', '#D4839B', '#FCEFBB'))+
+  scale_fill_manual(breaks = c("fQuad 1over", "aQuad 1under", "eQuad 2",
+                               "dQuad 3over", "cQuad 3under", "bQuad 4"), 
+                    values=c("#ccd1f8", "#fadade", "#7482ec",
+                             '#1c31ceee', '#b2182bee', '#ed7b89'))+
+  ylim(0,238) +
   xlab(" ")+
-  ylab("fraction")+
-  theme(legend.position = 'none', 
-        plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
-
-PO4.TP <- ggplot(new2, aes(x = BMPType, fill = quadrant)) +
-  geom_bar(width = 0.75,color = 'black') +
-  theme_classic(base_size = 10) +
-  scale_fill_manual(breaks = c("Quad 1over", "Quad 1under", "Quad 2",
-                               "Quad 3over", "Quad 3under", "Quad 4"), 
-                    values=c("#CCF1F6", "#8AD6E0", "#E2D1F1",
-                             '#FDDAE4', '#D4839B', '#FCEFBB'))+
-  xlab(" ")+
-  ylab("fraction")+
+  ylab("count")+
   theme(legend.position = 'none', 
         plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
 
 
-NO3.NH4 <- ggplot(new3, aes(x = BMPType, fill = quadrant)) +
+
+PO4.TP2 <- ggplot(new2, aes(x = BMPType, fill = quadrant)) +
   geom_bar(width = 0.75,color = 'black') +
   theme_classic(base_size = 10) +
-  scale_fill_manual(breaks = c("Quad 1over", "Quad 1under", "Quad 2",
-                               "Quad 3over", "Quad 3under", "Quad 4"), 
-                    values=c("#CCF1F6", "#8AD6E0", "#E2D1F1",
-                             '#FDDAE4', '#D4839B', '#FCEFBB'))+
+  scale_fill_manual(breaks = c("fQuad 1over", "aQuad 1under", "eQuad 2",
+                               "dQuad 3over", "cQuad 3under", "bQuad 4"), 
+                    values=c("#ccd1f8", "#fadade", "#7482ec",
+                             '#1c31ceee', '#b2182bee', '#ed7b89'))+
+  ylim(0,238) +
+  xlab(" ")+
+  ylab("count")+
+  theme(legend.position = 'none', 
+        plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
+
+
+NO3.NH42 <- ggplot(new3, aes(x = BMPType, fill = quadrant)) +
+  geom_bar(width = 0.75,color = 'black') +
+  theme_classic(base_size = 10) +
+  scale_fill_manual(breaks = c("fQuad 1over", "aQuad 1under", "eQuad 2",
+                               "dQuad 3over", "cQuad 3under", "bQuad 4"), 
+                    values=c("#ccd1f8", "#fadade", "#7482ec",
+                             '#1c31ceee', '#b2182bee', '#ed7b89'))+
+  ylim(0,238) +
   xlab(" ")+ 
-  ylab("fraction") +
+  ylab("count") +
+  theme(legend.position = 'none', 
+        plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
+
+
+
+TN.NO32 <- ggplot(new5, aes(x = BMPType, fill = quadrant)) +
+  geom_bar(width = 0.75, color = 'black') +
+  theme_classic(base_size = 10) +
+  scale_fill_manual(breaks = c("fQuad 1over", "aQuad 1under", "eQuad 2",
+                               "dQuad 3over", "cQuad 3under", "bQuad 4"), 
+                    values=c("#ccd1f8", "#fadade", "#7482ec",
+                             '#1c31ceee', '#b2182bee', '#ed7b89'))+
+  ylim(0,238) +
+  xlab(" ")+ 
+  ylab("count") +
+  theme(legend.position = 'none', 
+        plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
+
+
+TN.NH42 <- ggplot(new6, aes(x = BMPType, fill = quadrant)) +
+  geom_bar(width = 0.75, color = 'black') +
+  theme_classic(base_size = 10) +
+  scale_fill_manual(breaks = c("fQuad 1over", "aQuad 1under", "eQuad 2",
+                               "dQuad 3over", "cQuad 3under", "bQuad 4"), 
+                    values=c("#ccd1f8", "#fadade", "#7482ec",
+                             '#1c31ceee', '#b2182bee', '#ed7b89'))+
+  ylim(0,238) +
+  xlab(" ")+ 
+  ylab("count") +
   theme(legend.position = 'none', 
         plot.margin = margin(t=0.5,r = 0.3, b = 0.3, l = 0.3, unit = "cm"))
 
 
 
 
-plot_grid(inset, TN.TP, PO4.TP, NO3.NH4,
-          labels = c(" ","TN vs TP retention", "PO4 vs TP retention", 
-                     "NO3 vs NH4 retention"),
-          ncol = 2, rel_heights = c(1,1), label_size = 10)
 
+tiff(filename = "figures/Solute_retention_supp_unscaled.tif", height=4, width=6, units= "in", res=800, compression= "lzw")
 
+plot_grid(inset, TN.TP2, PO4.TP2, NO3.NH42, TN.NO32, TN.NH42,
+          labels = c("A) ","B) TN vs TP", "C) TP vs PO4 ", 
+                     "D) NH4 vs NO3" , "E) TN vs NO3", "F) TN vs NH4 "),
+          ncol = 3, rel_heights = c(1,1), label_size = 10, label_x = 0, hjust = 0)
+
+dev.off()
